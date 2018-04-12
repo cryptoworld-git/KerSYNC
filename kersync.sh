f3d5a04a6e3b0c0cb4f2cee4f3dfbceec28a8454
Beard <beardlyness@users.noreply.github.com>
2018-04-12 05:24:53 -0700

    0.1.1 Beta
    # date             :04-12-2018
    # version          :0.1.1 Beta

--- kersync.sh
+++ kersync.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #===============================================================================================================================================
-# (C) Copyright 2018 KerSYNC a project under the Crypto World Foundation (https://cryptoworld.is).
+# (C) Copyright 2018 Kersync a project under the CryptoWorld Foundation (https://cryptoworld.is).
 #
 # Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,13 +15,13 @@
 # limitations under the License.
 #
 #===============================================================================================================================================
-# title            :KerSYNC
+# title            :Kersync
 # description      :This script will make it super easy to rsync the kernel.org mirror.
-# author           :The Crypto World Foundation.
+# author           :The CryptoWorld Foundation.
 # contributors     :Beardlyness
-# date             :04-11-2018
-# version          :0.1.0 Beta
-# os               :Debian/Ubuntu
+# date             :04-12-2018
+# version          :0.1.1 Beta
+# os               :Debian
 # usage            :bash kersync.sh
 # notes            :If you have any problems feel free to email the maintainer: beard [AT] cryptoworld [DOT] is
 #===============================================================================================================================================
@@ -52,17 +52,33 @@
 
       echo -e "\033[92mChecking software list..\e[0m"
 
-    [ ! -x  /usr/bin/lsb_release ] || [ ! -x  /usr/bin/rsync ] || [ ! -x  /usr/bin/dialog ] ; then
+    [ ! -x  /usr/bin/lsb_release ] || [ ! -x  /usr/bin/wget ] || [ ! -x  /usr/bin/rsync ] || [ ! -x  /usr/bin/apt-transport-https ] || [ ! -x  /usr/bin/dirmngr ] || [ ! -x  /usr/bin/ca-certificates ] || [ ! -x  /usr/bin/dialog ] ; then
 
-      echo -e "\033[92mLsb_release: checking for software..\e[0m"
+      echo -e "\033[92mlsb_release: checking for software..\e[0m"
       echo -e "\033[34mInstalling lsb_release, Please Wait...\e[0m"
       apt-get install lsb-release
 
-      echo -e "\033[92mRsync: checking for software..\e[0m"
+      echo -e "\033[92mwget: checking for software..\e[0m"
+      echo -e "\033[34mInstalling wget, Please Wait...\e[0m"
+      apt-get install wget
+
+      echo -e "\033[92mrsync: checking for software..\e[0m"
       echo -e "\033[34mInstalling rsync, Please Wait...\e[0m"
       apt-get install rsync
 
-      echo -e "\033[92mDialog: checking for software..\e[0m"
+      echo -e "\033[92mapt-transport-https: checking for software..\e[0m"
+      echo -e "\033[34mInstalling apt-transport-https, Please Wait...\e[0m"
+      apt-get install apt-transport-https
+
+      echo -e "\033[92mdirmngr: checking for software..\e[0m"
+      echo -e "\033[34mInstalling dirmngr, Please Wait...\e[0m"
+      apt-get install dirmngr
+
+      echo -e "\033[92mca-certificates: checking for software..\e[0m"
+      echo -e "\033[34mInstalling ca-certificates, Please Wait...\e[0m"
+      apt-get install ca-certificates
+
+      echo -e "\033[92mdialog: checking for software..\e[0m"
       echo -e "\033[34mInstalling dialog, Please Wait...\e[0m"
       apt-get install dialog
   fi
@@ -91,8 +107,8 @@
             HEIGHT=20
             WIDTH=120
             CHOICE_HEIGHT=3
-            BACKTITLE="Kersync | Installer"
-            TITLE="~~~ Kersync ~~~"
+            BACKTITLE="KerSYNC | Installer"
+            TITLE="~~~ KerSYNC ~~~"
             MENU="Choose one of the following Web Servers:"
 
             OPTIONS=(1 "Apache"
@@ -119,8 +135,8 @@
             service apache2 status
             ;;
           2)
-          echo deb http://nginx.org/packages/$system/ $flavor nginx > /etc/apt/sources.list.d/engine.list
-          echo deb-src http://nginx.org/packages/$system/ $flavor nginx >> /etc/apt/sources.list.d/engine.list
+          echo deb http://nginx.org/packages/$system/ $flavor nginx > /etc/apt/sources.list.d/deb.nginx.org.list
+          echo deb-src http://nginx.org/packages/$system/ $flavor nginx >> /etc/apt/sources.list.d/deb.nginx.org.list
             wget -4 https://nginx.org/keys/nginx_signing.key
             apt-key add nginx_signing.key
           echo "Performing upkeep.."
@@ -130,13 +146,131 @@
             service nginx status
             ;;
           3)
-          echo deb http://mirror.tuxhelp.org/debian/ squeeze main > /etc/apt/sources.list.d/engine.list
+          echo deb http://mirror.tuxhelp.org/debian/ squeeze main > /etc/apt/sources.list.d/deb.nginx.org.list
             apt-key adv --recv-keys --keyserver keys.gnupg.net 79AF54A9
           echo "Performing upkeep.."
             upkeep
           echo "Installing Hiawatha.."
             apt-get install hiawatha
             service hiawatha status
+            ;;
+        esac
+    clear
+      ;;
+    [nN]|[nN][oO])
+      echo "You have said no? We cannot work without your permission!"
+      ;;
+    *)
+      echo "Invalid response. You okay?"
+      ;;
+esac
+
+# PHP handler Arg Third
+      read -r -p "Would you like to setup a PHP Handler to serve the content? (Y/N) " REPLY
+        case "${REPLY,,}" in
+          [yY]|[yY][eE][sS])
+            HEIGHT=20
+            WIDTH=120
+            CHOICE_HEIGHT=3
+            BACKTITLE="KerSYNC | Installer"
+            TITLE="~~~ KerSYNC ~~~"
+            MENU="Choose one of the following PHP Handlers:"
+
+            OPTIONS=(1 "PHP 5.6 | OLD-STABLE"
+                     2 "PHP 7.0 | STABLE"
+                     3 "PHP 7.X | BETA")
+
+            CHOICE=$(dialog --clear \
+                            --backtitle "$BACKTITLE" \
+                            --title "$TITLE" \
+                            --menu "$MENU" \
+                            $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                            "${OPTIONS[@]}" \
+                            2>&1 >/dev/tty)
+
+            clear
+
+# Attached Arg for dialogs $CHOICE output
+        case $CHOICE in
+          1)
+          echo deb https://packages.sury.org/php/ $flavor main > /etc/apt/sources.list.d/deb.sury.org.list
+            wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+          echo "Performing upkeep.."
+            upkeep
+          echo "Installing PHP 5.6 | OLD-STABLE.."
+            apt-get install php5.6-cli php5.6-fpm
+            service php5.6-fpm status
+            ;;
+          2)
+          echo deb https://packages.sury.org/php/ $flavor main > /etc/apt/sources.list.d/deb.sury.org.list
+            wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+          echo "Performing upkeep.."
+            upkeep
+          echo "Installing PHP 7.0 | STABLE.."
+            apt-get install php7.0-cli php7.0-fpm
+            service php7.0-fpm status
+            ;;
+          3)
+          echo deb https://packages.sury.org/php/ $flavor main > /etc/apt/sources.list.d/deb.sury.org.list
+            wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+          echo "Performing upkeep.."
+            upkeep
+          echo "Installing PHP 7.2 | BETA.."
+            apt-get install php7.2-cli php7.2-fpm
+            service php7.2-fpm status
+            ;;
+        esac
+    clear
+      ;;
+    [nN]|[nN][oO])
+      echo "You have said no? We cannot work without your permission!"
+      ;;
+    *)
+      echo "Invalid response. You okay?"
+      ;;
+esac
+
+# Database Server Arg Fourth
+      read -r -p "Would you like to setup a Database server to store the mirror linkbacks? (Y/N) " REPLY
+        case "${REPLY,,}" in
+          [yY]|[yY][eE][sS])
+            HEIGHT=20
+            WIDTH=120
+            CHOICE_HEIGHT=2
+            BACKTITLE="KerSYNC | Installer"
+            TITLE="~~~ KerSYNC ~~~"
+            MENU="Choose one of the following Database Servers:"
+
+            OPTIONS=(1 "MySQL"
+                     2 "MariaDB")
+
+            CHOICE=$(dialog --clear \
+                            --backtitle "$BACKTITLE" \
+                            --title "$TITLE" \
+                            --menu "$MENU" \
+                            $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                            "${OPTIONS[@]}" \
+                            2>&1 >/dev/tty)
+
+            clear
+
+# Attached Arg for dialogs $CHOICE output
+        case $CHOICE in
+          1)
+          echo "Performing upkeep.."
+            upkeep
+          echo "Installing MySQL Server.."
+            apt-get install mysql-server
+          echo "Launching Installer.."
+            mysql_secure_installation
+            ;;
+          2)
+          echo "Performing upkeep.."
+            upkeep
+            echo "Installing MariaDB Server.."
+              apt-get install mariadb-server
+            echo "Launching Installer.."
+              mysql_secure_installation
             ;;
         esac
     clear
